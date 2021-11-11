3a2e4767d8c4d053790166bb7ac298703e0a50e3
MatteoRuggieri2 <matteoruggieri2@gmail.com>
2021-11-11 15:47:25 +0100

    Inseriti gli input - js

---
+++ js/script.js
@@ -0,0 +1,32 @@
+// CONSEGNA
+
+// Chiedi all’utente il suo nome,
+// poi chiedi il suo cognome,
+// poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito21
+
+
+
+// VARIABLES
+// casualNumber-variable
+let casualNumber = 21;
+console.log('Casual Number: ' , casualNumber)
+
+
+// INPUT
+// Name Request
+const userName = prompt('Qual è il tuo nome?');
+console.log('Name: ' , userName)
+
+// Surname Request
+const userSurname = prompt('Qual è il tuo cognome?');
+console.log('Surname: ' , userSurname)
+
+// Color Request
+const color = prompt('Qual è il tuo colore preferito?');
+console.log('Color: ' , color)
+
+// LOGIC
+
+
+// OUTPUT
